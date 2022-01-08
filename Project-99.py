532ff7d0c5b3dc2bc5b09affa7a6da9fee55222a
ThunderCloud228 <83110279+ThunderCloud228@users.noreply.github.com>
2022-01-08 15:44:00 +0530

    Add files via upload

---
+++ Project-99.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+import time
+
+def main():
+    path = "/PATH_TO_DELETE"
+    days = 30
+    seconds = time.time() - (days * 24 * 60 * 60)
+    
+    if os.path.exists(path):
+        for root_folder, folders, files in os.walk(path):
+            if seconds >= get_file_or_folder_age(root_folder):
+                remove_folder(root_folder)
+
